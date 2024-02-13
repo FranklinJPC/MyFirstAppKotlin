7179dccc5758ee24edc5dfe9b0feb5f3ec22747d
Franklin Patiño <jhoelpatino26@hotmail.com>
2024-02-13 16:10:03 -0500

    Ejercicios Corregidos

--- app/src/main/java/com/JhzApp/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/JhzApp/myfirstapp/MainActivity.kt
@@ -11,6 +11,9 @@ import com.JhzApp.myfirstapp.databinding.ActivityMainBinding
 import android.widget.EditText;
 import android.widget.TextView;
 import android.view.View;
+import android.widget.ArrayAdapter
+import android.widget.Spinner;
+import android.widget.Toast;
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,6 +21,8 @@ class MainActivity : AppCompatActivity() {
     private lateinit var number_1: EditText;
     private lateinit var number_2: EditText;
     private lateinit var result: TextView;
+    private lateinit var spinner: Spinner;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -26,6 +31,12 @@ class MainActivity : AppCompatActivity() {
         number_1=findViewById(R.id.number_1);
         number_2=findViewById(R.id.number_2);
         result=findViewById(R.id.textResultado);
+
+        spinner = findViewById(R.id.spinner)
+        val opciones = arrayOf("sumar", "restar", "multiplicar", "dividir", "verificar")
+        val adapter = ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, opciones)
+        spinner.adapter = adapter
+
 
 
         val navView: BottomNavigationView = binding.navView
@@ -40,10 +51,41 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    fun sumar(view: View){
-        val n1=number_1.text.toString().toInt();
-        val n2=number_2.text.toString().toInt();
-        val suma=n1+n2;
-        result.setText(suma.toString());
+    fun operar(view: View)
+    {
+        val valor_1 = number_1.text.toString();
+        val valor_2 = number_2.text.toString();
+        val selector = spinner.selectedItem.toString();
+        if (selector.equals("sumar"))
+        {
+            val suma = valor_1.toInt() + valor_2.toInt();
+            result.setText(suma.toString());
+        }
+        else if(selector.equals("restar"))
+        {
+            val resta = valor_1.toInt() - valor_2.toInt();
+            result.setText(resta.toString());
+        }
+        else if(selector.equals("multiplicar"))
+        {
+            val mult = valor_1.toInt() * valor_2.toInt();
+            result.setText(mult.toString());
+        }
+        else if(selector.equals("dividir"))
+        {
+            val dividir = valor_1.toInt() / valor_2.toInt();
+            result.setText(dividir.toString());
+        }
+        else if(selector.equals("verificar"))
+        {
+            val tamanio = number_2.text.toString();
+            val tam2 = tamanio.length;
+            if(tam2 == 0)
+            {
+                val noti = Toast.makeText(this, "Campos vacios", Toast.LENGTH_LONG)
+                noti.show()
+            }
+        }
+
     }
 }
